3bcc053af92a4265eea06dac93254c894c7bd5b2
fiyinfoluwa <fiyinfoluwaoyekan@gmail.com>
2025-04-06 22:16:09 +0100

    I just worked on the search feature, so we can search for phone numbers

--- phonebookapi/contacts/views.py
+++ phonebookapi/contacts/views.py
@@ -1,10 +1,12 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Contact
 from .serializers import ContactSerializer
 
 class ContactViewSet(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'phone_number']   #will enable search by name or phone number
 
     def get_queryset(self):
         return Contact.objects.filter(user=self.request.user)
